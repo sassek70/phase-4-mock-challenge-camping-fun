d391bf83aebae0d0288beeaea20756060b1e4e95
Kevin Sasse <sassek70@gmail.com>
2022-11-29 23:48:56 -0500

    added numericality option

--- app/models/camper.rb
+++ app/models/camper.rb
@@ -1,6 +1,7 @@
 class Camper < ApplicationRecord
     validates :name, presence: true
-    validates :age, inclusion: 8..18
+    # validates :age, inclusion: 8..18
+    validates :age, numericality: {greater_than_or_equal_to: 18, less_than_or_equal_to: 65 }
     has_many :signups
     has_many :activities, through: :signups
 end
